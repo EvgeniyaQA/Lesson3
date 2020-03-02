9c2aeeb39a6e6e3268633d5601d517acc31eab42
Anna Jarmolchuk <prez2017@gmail.com>
2020-03-02 21:03:18 +0200

    Initial commit

---
+++ src/main/java/Lesson2.java
@@ -0,0 +1,47 @@
+public class Lesson2 {
+    public static void main(String[] args) {
+        System.out.println("Zhenya");
+        int a = 2;
+        int b = 3;
+        int k =3;
+        int d =4;
+
+        ++k;
+        --d;
+        System.out.println(k);
+        System.out.println(d);
+
+       /* int sum =1;
+        int dif = 2;
+        float divide = 3;
+        long multiply = 4;
+        sum+=a;
+        dif -=a;
+        divide /=a;
+        multiply *=a;
+        System.out.println("Result with adding : " +sum);
+        System.out.println("Variable type dif : " +dif);
+        System.out.println("Variable type dif : " +divide);
+        System.out.println("Variable type dif : " +multiply);*/
+
+
+        /*int f = a+b;
+        long multipleResult = a*b;
+        int vychitanie = a-b;
+        double divide = a/b;
+        System.out.println("Переменная со сложения: "  + f);
+        System.out.println("-------------------------------");
+        System.out.println("Результат умножения:  " + multipleResult);
+        System.out.println("-------------------------------");
+        System.out.println("Результат вычитания : " + vychitanie);
+        System.out.println("-------------------------------");
+        System.out.println( "Result of dividing: " + divide);*/
+
+        int v = 5;
+        int z = 6;
+        if(v==z){
+            System.out.println("Equals");
+        }
+    }
+}
+
---
+++ src/main/java/Lesson3.java
@@ -0,0 +1,61 @@
+public class Lesson3 {
+    public static void main(String[] args) {
+        int a= 3;
+        int b = 4;
+        doMultiplicate(a,b);
+    }
+    public static void doMultiplicate(int a, int b) {
+        int c = a * b;
+        System.out.println("Before" + c + a + "Result");
+    }}
+       /* switch (number) {
+            case 1:
+                System.out.println("Here's number 1");
+                break;
+            case 2:
+                System.out.println("Here's number 2");
+                break;
+            case 4:
+                System.out.println("Here we go");
+                break;
+            case 5:
+                System.out.println("Here's number 5");
+                break;
+            default:
+                System.out.println("There was no needed number");*/
+        
+        /*double a = 5.01;
+        double b = 5.02;
+        if (a != b) {
+            System.out.println("Equals");
+        } else if (a == b || a < b){
+            System.out.println("Not equals");
+        } else {
+            System.out.println("FUCK");
+        }*/
+
+
+      /*  int num =3;
+        int output = 0;
+        switch (num) {
+            case 1:
+                output = 3;
+                break;
+            case 2:
+                case 3:
+                case 4:
+                output = 6;
+                break;
+            case 5:
+                output = 12;
+                break;
+            default:
+                output = 24;
+        }
+        System.out.println(output);
+        */
+      /*  int x = 3;
+        int y = 2;
+        int z = x < y ? (x + y) : (x - y);
+        System.out.println(z);*/
+
